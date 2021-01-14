a1246b1c3f57ff55752616b4b9007db5d64703fe
Baraka Mulumia <barakamulumia@gmail.com>
2021-01-14 09:19:20 +0300

    hashing passwords

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express"),
   cors = require("cors"),
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  bcrypt = require("bcrypt");
 
 const { MySqlConnection } = require("mysqlconnector");
 
 const app = express();
 const port = 8080;
+const saltRounds = 10;
 
 const db = new MySqlConnection("localhost", "root", "1NF1N1T7_X", "authdb");
 
@@ -24,33 +26,42 @@ app.use(cors());
 app.post("/register", (req, res, next) => {
   const { id, username, firstName, lastName, password } = req.body;
 
-  db.connectAsync().then(() => {
-    let sql = `INSERT INTO users (userId, firstname, lastname, username, password) VALUES ('${id}', '${firstName}', '${lastName}', '${username}','${password}')`;
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
+    db.connectAsync().then(() => {
+      let sql = `INSERT INTO users (userId, firstname, lastname, username, password) VALUES ('${id}', '${firstName}', '${lastName}', '${username}','${hash}')`;
 
-    db.queryAsync(sql)
-      .then(() => {
-        console.log("send to db successfull");
-      })
-      .catch((e) => {
-        res.send({ err: e });
-        next(e);
-      });
+      db.queryAsync(sql)
+        .then(() => {
+          console.log("send to db successfull");
+        })
+        .catch((e) => {
+          res.send({ err: e });
+          next(e);
+        });
+    });
   });
 });
 
 app.post("/login", (req, res, next) => {
   const { password, username } = req.body;
   db.connectAsync().then(() => {
-    let sql = `SELECT * FROM users  WHERE username='${username}' AND password='${password}'`;
+    let sql = `SELECT * FROM users  WHERE username='${username}'`;
     db.queryAsync(sql).then(
       (response) => {
-        if (response.length) {
-          console.log(response);
-          res.json(response);
+        if (response.length > 0) {
+          bcrypt.compare(password, response[0].password, (err, result) => {
+            if (result) {
+              res.json(response);
+            } else {
+              res.send({ message: "Wrong details" });
+            }
+          });
         } else {
-          res.send({ message: "Wrong details" });
+          res.send({ message: "user doesn't exist" });
         }
-        next();
       },
       (error) => {
         console.log(error);
